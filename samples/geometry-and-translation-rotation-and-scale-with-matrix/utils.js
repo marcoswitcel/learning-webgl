34276c978e2a43eeeec2873a867227cd3784108b
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-02-23 21:28:47 -0300

    projection

--- samples/geometry-and-translation-rotation-and-scale-with-matrix/utils.js
+++ samples/geometry-and-translation-rotation-and-scale-with-matrix/utils.js
@@ -91,4 +91,13 @@ export class Mat3 {
           0, 0, 1,
         ];
     }
+
+    static projection(width, height) {
+        // Note: This matrix flips the Y axis so that 0 is at the top.
+        return [
+          2 / width, 0, 0,
+          0, -2 / height, 0,
+          -1, 1, 1
+        ];
+    }
 };
