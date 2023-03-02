7b2f7f21b22dc0e3f40aca5b487a3200c09e9764
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-03-01 22:24:15 -0300

    renderizando

--- samples/geometry-3d-f-orthographic/index.js
+++ samples/geometry-3d-f-orthographic/index.js
@@ -1,5 +1,5 @@
 import { createProgram, createShader } from './gl-utils.js';
-import { createCanvas, fetchTex, Mat3 } from './utils.js';
+import { createCanvas, degToRad, fetchTex, Mat4 } from './utils.js';
 
 const canvas = createCanvas(600, 500, document.body);
 
@@ -16,19 +16,20 @@ const main = (vertexShaderSource, fragmentShaderSource) => {
   
   const program = createProgram(gl, vertexShader, fragmentShader);
 
-  const translation = [0, 0];
-  const rotation = [0, 1];
-  const angleInRadians = (360 - 1) * Math.PI / 180;
-  const scale = [1, 1];
+  const translation = [45, 150, 0];
+  const rotation = [degToRad(40), degToRad(25), degToRad(325)];
+  const scale = [1, 1, 1];
   const width = 100;
   const height = 30;
   const color = [Math.random(), Math.random(), Math.random(), 1];
   
   // Compute the matrix
-  let matrix = Mat3.projection(gl.canvas.clientWidth, gl.canvas.clientHeight);
-  matrix = Mat3.translate(matrix, translation[0], translation[1]);
-  matrix = Mat3.rotate(matrix, angleInRadians);
-  matrix = Mat3.scale(matrix, scale[0], scale[1]);
+  let matrix = Mat4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
+  matrix = Mat4.translate(matrix, translation[0], translation[1], translation[2]);
+  matrix = Mat4.xRotate(matrix, rotation[0]);
+  matrix = Mat4.yRotate(matrix, rotation[1]);
+  matrix = Mat4.zRotate(matrix, rotation[2]);
+  matrix = Mat4.scale(matrix, scale[0], scale[1], scale[2]);
 
   const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
   const matrixLocation = gl.getUniformLocation(program, 'u_matrix');
@@ -50,7 +51,7 @@ const main = (vertexShaderSource, fragmentShaderSource) => {
   gl.useProgram(program);
 
   // Set the matrix.
-  gl.uniformMatrix3fv(matrixLocation, false, matrix);
+  gl.uniformMatrix4fv(matrixLocation, false, matrix);
 
   gl.enableVertexAttribArray(positionAttributeLocation);
 
@@ -61,7 +62,7 @@ const main = (vertexShaderSource, fragmentShaderSource) => {
   //setRectangle(gl, translation[0], translation[1], width, height);
  
   // Configura o atributo para saber como extrair dados do buffer array
-  const size = 2;          // 2 components per iteration
+  const size = 3;          // 3 components per iteration
   const type = gl.FLOAT;   // the data is 32bit floats
   const normalize = false; // don't normalize the data
   const stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
@@ -102,29 +103,28 @@ function setGeometry(gl) {
     gl.ARRAY_BUFFER,
     new Float32Array([
       // left column
-      0, 0,
-      30, 0,
-      0, 150,
-      0, 150,
-      30, 0,
-      30, 150,
+      0,   0,  0,
+      30,   0,  0,
+      0, 150,  0,
+      0, 150,  0,
+      30,   0,  0,
+      30, 150,  0,
 
       // top rung
-      30, 0,
-      100, 0,
-      30, 30,
-      30, 30,
-      100, 0,
-      100, 30,
+      30,   0,  0,
+      100,   0,  0,
+      30,  30,  0,
+      30,  30,  0,
+      100,   0,  0,
+      100,  30,  0,
 
       // middle rung
-      30, 60,
-      67, 60,
-      30, 90,
-      30, 90,
-      67, 60,
-      67, 90,
-    ]),
+      30,  60,  0,
+      67,  60,  0,
+      30,  90,  0,
+      30,  90,  0,
+      67,  60,  0,
+      67,  90,  0]),
     gl.STATIC_DRAW);
 }
 
