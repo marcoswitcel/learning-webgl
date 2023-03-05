7c9de86f07caefb205b08606c343a05896046402
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-03-04 21:34:03 -0300

    usando w

--- samples/geometry-3d-f-perspective/vertex-shader.glsl
+++ samples/geometry-3d-f-perspective/vertex-shader.glsl
@@ -17,7 +17,7 @@ void main() {
   float zToDivideBy = 1.0 + position.z * u_fudgeFactor;
 
   // Divide x and y by z.
-  gl_Position = vec4(position.xy / zToDivideBy, position.zw);
+  gl_Position = vec4(position.xyz, zToDivideBy);
 
   v_color = a_color;
 }
