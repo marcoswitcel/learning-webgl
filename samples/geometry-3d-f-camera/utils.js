337818c0d38c3a38f273a2e757dac0edc8c078b0
Joao Marcos <joao.marcos.witcel@gmail.com>
2023-03-06 23:18:26 -0300

    normalize, dotproduct e lookat

--- samples/geometry-3d-f-camera/utils.js
+++ samples/geometry-3d-f-camera/utils.js
@@ -116,6 +116,23 @@ export class Mat3 {
 
 export class Mat4 {
 
+    lookAt(cameraPosition, target, up) {
+        const zAxis = normalize(
+            subtractVectors(cameraPosition, target));
+        const xAxis = normalize(cross(up, zAxis));
+        const yAxis = normalize(cross(zAxis, xAxis));
+     
+        return [
+           xAxis[0], xAxis[1], xAxis[2], 0,
+           yAxis[0], yAxis[1], yAxis[2], 0,
+           zAxis[0], zAxis[1], zAxis[2], 0,
+           cameraPosition[0],
+           cameraPosition[1],
+           cameraPosition[2],
+           1,
+        ];
+    }
+
     static perspective(fieldOfViewInRadians, aspect, near, far) {
         const f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
         const rangeInv = 1.0 / (near - far);
@@ -391,4 +408,26 @@ export function makeZToWMatrix(fudgeFactor) {
         0, 0, 1, fudgeFactor,
         0, 0, 0, 1,
     ];
+}
+
+export function cross(a, b) {
+    return [
+        a[1] * b[2] - a[2] * b[1],
+        a[2] * b[0] - a[0] * b[2],
+        a[0] * b[1] - a[1] * b[0]
+    ];
+}
+
+export function subtractVectors(a, b) {
+    return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+
+export function normalize(v) {
+    const length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+    // make sure we don't divide by 0.
+    if (length > 0.00001) {
+      return [v[0] / length, v[1] / length, v[2] / length];
+    } else {
+      return [0, 0, 0];
+    }
 }
